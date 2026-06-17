f34560b85729c02f0d1b7a7974d3e3d43380ad13
tai-namechan <76046768+tai-namechan@users.noreply.github.com>
2026-06-17 16:29:40 +0000

    ユーザー削除エンドポイント実装

--- exercises/01-fastapi-sample/sql_app/main.py
+++ exercises/01-fastapi-sample/sql_app/main.py
@@ -71,6 +71,16 @@ def read_user(
     return db_user
 
 
+@app.delete("/users/{user_id}", status_code=204)
+def delete_user(
+    user_id: int, db: Session = db_session, _: models.User = current_user_dep
+) -> None:
+    db_user = crud.get_user(db, user_id=user_id)
+    if db_user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    crud.delete_user(db=db, user=db_user)
+
+
 @app.get("/users/{user_id}/items", response_model=List[schemas.Item])
 def read_items_for_user(
     user_id: int,
